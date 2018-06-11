01f98f95a9fe0f31ce62a4ba891dae15a215fd8b
AlCast <alvaro.castelan.21@gmail.com>
2018-06-10 21:57:58 -0500

    added scoring logic

--- public/js/scoring.js
+++ public/js/scoring.js
@@ -2,43 +2,19 @@
 
 $(document).ready(function() {
 
-   
+
  // Code to get the users answers from database and get ready to be compared 
- var winningTeam;
- console.log(winningTeam);
- var homePitcher5SOs;
- console.log(homePitcher5SOs);
- var awayTeamHits;
- console.log(awayTeamHits);
- var homeTeam7runs; 
- console.log(homeTeam7runs);
- var awayTeam2Errors; 
- console.log(awayTeam2Errors);
-// user answers 
- var gameIdNeededToCompareNoParen;
- console.log(gameIdNeededToCompareNoParen);
- var answertoQ1NoParen;
- console.log(answertoQ1NoParen);
- var answertoQ2NoParen;
- console.log(answertoQ2NoParen);
- var answertoQ3NoParen;
- console.log(answertoQ3NoParen);
- var answertoQ4NoParen;
- console.log(answertoQ4NoParen);
- var answertoQ5NoParen;
- console.log(answertoQ5NoParen);
+
+
 
 
  $.get("/api/answersKey").then(function (data) {
-        // actual results
-    
-        //console.log(data)
-        //get the game id 
+ 
         var answersToCompare = data[0].games.split(",")
         var gameIdNeededToCompare = answersToCompare[0].split(":") 
         var idToUse = gameIdNeededToCompare[1];
         
-       gameIdNeededToCompareNoParen = idToUse.substr(1).slice(0,-1);
+        gameIdNeededToCompareNoParen = idToUse.substr(1).slice(0,-1);
         // console.log(gameIdNeededToCompareNoParen);
         //==========================================================
         // get the user answer of who won game 
@@ -52,7 +28,7 @@ $(document).ready(function() {
         //get the user answer home pitcher will have more than 5 strike outs
         var q2ToCompare =answersToCompare[2].split(":");
         var userAnswertoQ2 = q2ToCompare[1];
-        answertoQ2NoParen = userAnswertoQ2.substr(1).slice(0,-1);
+         answertoQ2NoParen = userAnswertoQ2.substr(1).slice(0,-1);
         // console.log(answertoQ2NoParen);
         //===========================================================
          //get the user answer how many innings will away pitcher have 
@@ -94,7 +70,11 @@ $(document).ready(function() {
 
  
 
-
+    var winningTeam;
+    
+    var homeTeam7runs;
+    var awayTeam2Errors;
+    var awayTeamHits;
 
     $.ajax({
         url: proxyUrl + query,
@@ -113,13 +93,14 @@ $(document).ready(function() {
         
         // check to see who won game
         if (awayTeamRuns > homeTeamRuns){
-        winningTeam = awayTeam
+        winningTeam = awayTeam;
         }
         else {
-            var winningTeam = homeTeam;
+        winningTeam = homeTeam;
         }; 
-        // get the amount of away team hits 
         
+        // get the amount of away team hits 
+        ;
         awayTeamHits = response.game.away.hits;
         
         //get home team runs 
@@ -145,28 +126,64 @@ $(document).ready(function() {
 
 
     
+    
     $.ajax({
         url: proxyUrl2 + query2,
         method: "GET"
     }).then(function (response) {
         
+        
         setTimeout(function(){
             var stats = response.game.home.players
             for (i = 0; i <stats.length; i++){
                 var postion = response.game.home.players[i].primary_position;
+                
+            
+                
                 if (postion === "SP"){
-                    
-                    homePitcher5SOs = response.game.home.players[i].statistics.pitch_metrics.overall.outs.ktotal;
-                    console.log("worked 2");
+                 var homePitcher5SOs = response.game.home.players[i].statistics.pitch_metrics.overall.outs.ktotal; 
                 }
-            }}, 1001)
-            console.log("worked 1");
-        
+            }}, 2000);
+            
+            var score = 0; 
+            console.log(gameIdNeededToCompareNoParen);
+            console.log(answertoQ1NoParen);
+            console.log(answertoQ2NoParen);
+            console.log(answertoQ3NoParen);
+            console.log(answertoQ4NoParen);
+            console.log(answertoQ5NoParen);
+            console.log(winningTeam);
+            console.log(awayTeam2Errors);
+            console.log(awayTeamHits);
+            console.log(homeTeam7runs);
+            if (answertoQ1NoParen = winningTeam){
+                score +=1;
+            };
+            // not working currently
+            // if (answertoQ2NoParen = homePitcher5SOs ) {
+            //     score +=1;
+            // };
+            if (answertoQ3NoParen = awayTeamHits){
+                score += 1; 
+            };
+            if(answertoQ4NoParen = homeTeam7runs){
+                score +=1; 
+            };
+            if(answertoQ5NoParen = awayTeam2Errors){
+                score +=1;
+            };
 
+            
+            console.log("Score: " + score);
+            
+            
+            
+            
     });
+    
 
  });
-
+ 
 });
 
 
